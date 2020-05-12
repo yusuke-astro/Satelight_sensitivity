2cd4db4650245c1dd9f9b9229de5d3d411fd8f98
yusuke-astro <48955483+yusuke-astro@users.noreply.github.com>
2020-05-12 15:08:13 +0900

    update

--- sensitivity04.py
+++ sensitivity04.py
@@ -1,5 +1,6 @@
 """
-このプログラムはsensitivity02.pyでP_optを求めた計算から引き続きNEPなどを求めるようにしたコードである．
+このプログラムはsensitivity03.pyでP_optを求めた計算から引き続きSensitivityなどを求めるようにしたコードである．
+switchによるDの制御をfor loop内のiの制御に置き換えた．
 """
 
 import  numpy as np
@@ -20,83 +21,11 @@ h = 6.62607004*10**(-34)
 k_B = 1.38064852*10**(-23)
 c = 299792458
 
-print("Define Pixel_pitch(D)...")
-print("If you use D=16 -->> input 0, D=24 -->> input 1")
-switch = 1 #= int(input(">>>"))
-"""
-def define_D(f):
-    #周波数に対してDが2つ存在するところで，使用するDの値を24にするか16にするかを決める仮想パラメータ
-    #switch=1なら78,68,89GHzにおいてD=24.0でswitch=0ならD=16.0が選択される
-    global switch
-    if f == 40*GHz:
-        D = 24.0
-    if f == 60*GHz:
-        D = 24.0
-    if f == 78*GHz:
-        if switch == 1:
-            D = 24.0
-        if switch == 0:
-            D = 16.0
-    if f == 50*GHz:
-        D = 24.0
-    if f == 68*GHz:
-        if switch == 1:
-            D = 24.0
-        if switch == 0:
-            D = 16.0
-    if f == 89*GHz:
-        if switch == 1:
-            D = 24.0
-        if switch == 0:
-            D = 16.0
-    if f == 100*GHz:
-        D = 16.0
-    if f == 119*GHz:
-        D = 16.0
-    if f == 140*GHz:
-        D = 16.0
-    return D
-
-def define_Num_dat(f):
-    global switch
-    if f == 40*GHz:
-        Num_det = 64.
-    if f == 60*GHz:
-        Num_det = 64.
-    if f == 78*GHz:
-        if switch == 1:
-            Num_det = 64.
-        if switch == 0:
-            Num_det = 144.
-    if f == 50*GHz:
-        Num_det = 64.
-    if f == 68*GHz:
-        if switch == 1:
-            Num_det = 64.
-        if switch == 0:
-            Num_det = 144.
-    if f == 89*GHz:
-        if switch == 1:
-            Num_det = 64.
-        if switch == 0:
-            Num_det = 144.
-    if f == 100*GHz:
-        Num_det = 144.
-    if f == 119*GHz:
-        Num_det = 144.
-    if f == 140*GHz:
-        Num_det = 144.
-    return Num_det
-"""
 def P_CMB(f):
-    global F
-    #D = define_D(F)
     T = 2.725
     return st.ita_HWP_LFT(F)*st.ita_Apt_LFT(f,D)*st.ita_Mir(f)*st.ita_Mir(f)*st.ita_2K(f)*st.ita_Lenslet(f)*st.ita_det_LFT()*st.Bose(f,T) * 1.000
 
 def P_HWP(f):
-    global F
-    #D = define_D(F)
     T = 20.00
     T_r = 5.000
     HWP_e = st.ita_Apt_LFT(f,D)*st.ita_Mir(f)*st.ita_Mir(f)*st.ita_2K(f)*st.ita_Lenslet(f)*st.ita_det_LFT()*st.Bose(f,T) * st.epsi_HWP_LFT(F)
@@ -104,13 +33,10 @@ def P_HWP(f):
     return HWP_e + HWP_r
 
 def P_Ape(f):
-    global F
-    #D = define_D(F)
     T = 5.000
     return st.ita_5K(f,D)*st.ita_Mir(f)*st.ita_Mir(f)*st.ita_2K(f)*st.ita_Lenslet(f)*st.ita_det_LFT()*st.Bose(f,T)
 
 def P_m1(f):
-    global F
     T = 5.000
     Pri = st.ita_Mir(f)*st.ita_2K(f)*st.ita_Lenslet(f)*st.ita_det_LFT()*st.Bose(f,T)
     Pri_e = Pri * st.epsi_Mir(f)
@@ -118,7 +44,6 @@ def P_m1(f):
     return Pri_e + Pri_r
 
 def P_m2(f):
-    global F
     T = 5.000
     Sec = st.ita_2K(f)*st.ita_Lenslet(f)*st.ita_det_LFT()*st.Bose(f,T)
     Sec_e = Sec * st.epsi_Mir(f)
@@ -126,13 +51,10 @@ def P_m2(f):
     return Sec_e + Sec_r
 
 def P_20K(f):
-    global F
-    #D = define_D(F)
     T = 20.0
     return st.ita_20K(F,D)*st.ita_2K(f)*st.ita_Lenslet(f)*st.ita_det_LFT()*st.Bose(f,T) * 1.000
 
 def P_2KF(f):
-    global F
     T = 2.000
     T_r = 0.100
     Flt2K_e = st.ita_Lenslet(f)*st.ita_det_LFT()*st.Bose(f,T) * st.epsi_2KF(f)
@@ -140,7 +62,6 @@ def P_2KF(f):
     return Flt2K_e + Flt2K_r
 
 def P_Lens(f):
-    global F
     T = 0.100
     T_r = 5.000
     Lens_e = st.ita_det_LFT()*st.Bose(f,T) * st.epsi_Lenslet(f)
@@ -160,14 +81,9 @@ def in_nep_ph(f):
     return 2.0*h*f*P_Opt(f) + 2.0*(P_Opt(f))**2
 
 def nep_ph(f):
-    global F
-    global BW
-    #print("F=",F,BW)
     return np.sqrt((integrate.quad(in_nep_ph, F-BW, F+BW)[0]))
 
 def nep_g(P_opt):
-    global F
-    global BW
     T_b = 0.100
     T_c = 0.171
     n = 3.0
@@ -186,10 +102,12 @@ def nep_int(a,b,c):
 
 
 def dPdT_CMB(f):
+    def eta(f):
+        return st.ita_HWP_LFT(F)*st.ita_Apt_LFT(f,D)*st.ita_Mir(f)*st.ita_Mir(f)*st.ita_2K(f)*st.ita_Lenslet(f)*st.ita_det_LFT()
+
     T_cmb = 2.725
     kT = k_B*T_cmb
-
-    return (1.0/k_B) * (( (h*f) / (T_cmb*(np.exp((h*f)/kT)-1.0)))**2) * np.exp((h*f)/kT)
+    return (eta(f)/k_B) * (( (h*f) / (T_cmb*(np.exp((h*f)/kT)-1.0)))**2) * np.exp((h*f)/kT)
 
 freq = np.array([40,60,78,50,68,89,68,89,119,78,100,140])*GHz
 
@@ -205,13 +123,15 @@ dPdT = []
 NET_det = []
 NET_arr = []
 sigma_s = []
-t_obs = 94672800.*0.72
-with open('sensitivity03.csv', 'w') as FILE:
+t_obs = 94672800.*0.95*0.95
+with open('sensitivity04.csv', 'w') as FILE:
     print("Freq[GHz],Band_width[GHz],D(Pixel_Pich),P_CMB,P_HWP,P_APT,P_20K,P_m1,P_m2,P_2KF,P_Lens,P_Det,P_opt,\
-    INTEG_P_CMB,INTEG_P_opt[p],NEP_ph[a],NEP_g[a],NEP_read[a],NEP_int[a],NEP_ext[a],NEP_det[a],dPdT_CMB,NET_det[u],NET_arr[u],sigma_s[u]",file=FILE)
+    INTEG_P_CMB,INTEG_P_opt[p],NEP_ph[a],NEP_g[a],NEP_read[a],NEP_int[a],NEP_ext[a],NEP_det[a],NET_det[u],NET_arr[u],dPdT_CMB,sigma_s[u]",file=FILE)
     for i in range(len(freq)):
         F = freq[i]
-        #D = define_D(F)
+        WL = c/freq[i]
+        BW = st.width(F)
+
         if i <= 5:
             D = 24.0
             N_dat = 64.
@@ -219,9 +139,6 @@ with open('sensitivity03.csv', 'w') as FILE:
             D = 16.0
             N_dat = 144.
 
-
-        WL = c/freq[i]
-        BW = st.width(F)
         INTEGRATE.append(integrate.quad(P_Opt, F-BW, F+BW)[0])
         NEP_ph.append(nep_ph(freq[i]))
         NEP_g.append(nep_g(INTEGRATE[i]))
@@ -232,19 +149,46 @@ with open('sensitivity03.csv', 'w') as FILE:
         dPdT.append(integrate.quad(dPdT_CMB, F-BW, F+BW)[0])
         NET_det.append(NEP_det[i]/(np.sqrt(2.)*dPdT[i]))
         NET_arr.append(NET_det[i]/np.sqrt(N_dat*0.8))
-        sigma_s.append(np.sqrt((4.*np.pi*1.0*2.*NET_arr[i]**2)/t_obs)*(10800./np.pi))
+        sigma_s.append(np.sqrt((4.*np.pi*1.0 * 2. * NET_arr[i]**2)/t_obs)*(10800./np.pi))
 
         print("{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}".format(F/GHz,BW/GHz,D,P_CMB(freq[i]),P_HWP(freq[i]),P_Ape(freq[i]),\
         P_20K(freq[i]),P_m1(freq[i]),P_m2(freq[i]),P_2KF(freq[i]),P_Lens(freq[i]),P_Det(freq[i]),P_Opt(freq[i]),\
         integrate.quad(P_CMB, F-BW, F+BW)[0],INTEGRATE[i]/p,NEP_ph[i]/a,NEP_g[i]/a,NEP_read[i]/a,NEP_int[i]/a,NEP_ext[i]/a,\
-        NEP_det[i]/a,dPdT[i],NET_det[i]/u,NET_arr[i]/u,sigma_s[i]/u) ,file=FILE)
-INTEGEATE = np.array(INTEGRATE)
-NEP_ph = np.array(NEP_ph)
-NEP_g = np.array(NEP_g)
-NET_arr
-sigma_s
+        NEP_det[i]/a,NET_det[i]/u,NET_arr[i]/u,dPdT[i],sigma_s[i]/u) ,file=FILE)
 
-
-df = pd.read_csv("sensitivity03.csv")
+df = pd.read_csv("sensitivity04.csv")
 df
-print(df)
+NET_overlaped_78GHz = np.sqrt(1./(1./(df.loc[2][15:23]**2 + df.loc[7][15:23]**2)).sum())
+NET_overlaped_78GHz
+
+NET_overlaped_68GHz = np.sqrt(1./(1./(df.loc[4][15:23]**2 + df.loc[6][15:23]**2)).sum())
+NET_overlaped_89GHz = np.sqrt(1./(1./(df.loc[5][15:23]**2 + df.loc[7][15:23]**2)).sum())
+
+
+hasebe_GHz = [40,
+50,
+60,
+68,
+78,
+89,
+100,
+119,
+140]
+
+hasebe_sensi = [39.76,
+25.76,
+20.69,
+12.72,
+10.39,
+8.95,
+6.43,
+4.3,
+4.43]
+plt.figure()
+plt.title("Sensitivity")
+plt.xlabel("Frequency[GHz]")
+plt.ylabel("\sigma_s")
+plt.plot(df["Freq[GHz]"],df["sigma_s[u]"],"o",color="red",label="takase", alpha=0.5)
+plt.plot(hasebe_GHz,hasebe_sensi,"o",color="blue",label="hasebe-san", alpha=0.5)
+plt.legend()
+plt.grid()
